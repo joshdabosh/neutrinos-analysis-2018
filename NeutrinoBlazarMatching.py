e363799718d35e6ba3bd0bf6a7abdf59c3841fd0
Joshua Wang <jwanggt@gmail.com>
2019-01-13 17:27:12 -0500

    update

--- NeutrinoBlazarMatching.py
+++ NeutrinoBlazarMatching.py
@@ -1,4 +1,4 @@
-from math import *
+from math import pi
 import json
 
 """
@@ -11,7 +11,6 @@ try:
     NtrDE = float(input('Enter the Declination of the neutrino event: '))
 
     blazar1 = [{"ra":56.24730, "de" : -85.456, "a": 'bobitybog', "z" : 0.789},{"ra": 186.24730, "de" : 83.456, "a": 'bobiasdfog', "z" : 2.21}]
-    #z = velocity/c, velocity = hubbleconstant(roughly 70)*distance
 
     listBlzInfo = []
     for blazar in blazar1:
@@ -26,14 +25,14 @@ try:
         velocity = z * 2997992458
         radius = velocity/70
 
-        RADistance = difRA/360 * radius * 2 * 3.1415
-        DEDistance = difDE/360 * radius * 2 * 3.1415
+        RADistance = difRA/360 * radius * 2 * math.pi
+        DEDistance = difDE/360 * radius * 2 * math.pi
 
         distance = (RADistance ** 2 + DEDistance ** 2) ** 0.5
         listBlzInfo.append({"dist" : distance, "name": BlzName})
 
-        smallestdistance = sorted(listBlzInfo, key=lambda x: x["dist"])[0]
-        print(smallestdistance)
+    smallestdistance = sorted(listBlzInfo, key=lambda x: x["dist"])[0]
+    print(smallestdistance)
 
 except Exception as e:
     print(e)
