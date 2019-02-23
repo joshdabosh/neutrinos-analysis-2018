8dfb28497472c8ee7cfd33601f8478f63dec6da7
zgrimsniper <43507498+zgrimsniper@users.noreply.github.com>
2019-02-23 12:17:11 -0500

    Add files via upload
    2/23

--- NeutrinoBlazarMatching.py
+++ NeutrinoBlazarMatching.py
@@ -1,34 +1,54 @@
-from math import pi
+from math import *
 import json
 
+"""
 blazar1 = open("blazar1.json").read()
 blazar1 = json.loads(blazar1)
+"""
 
-try:
-    NtrRA = float(input('Enter the Right Ascension of the neutrino event: '))
-    NtrDE = float(input('Enter the Declination of the neutrino event: '))
+def RatoLon(x):
+    h,m,s = x.split()
+    return 15*(int(h)+ int(m)/60 + int(s)/3600)
 
-    listBlzInfo = []
-    for blazar in blazar1:
-        BlzRA = float(blazar["ra"])
-        BlzDE = float(blazar["de"])
-        BlzName = blazar["a"]
+NtrRA = float(input('Enter the Right Ascension of the neutrino event: '))
+NtrDE = float(input('Enter the Declination of the neutrino event: '))
 
-        difRA = NtrRA - BlzRA
-        difDE = NtrDE - BlzDE
-        z = blazar['z']
+blazar1 = [{"ra":"2 34 45", "de" : -85.456, "a": 'bobitybog', "z" : 0.789},{"ra": "2 45 45", "de" : 83.456, "a": 'bobiasdfog', "z" : 2.21}]
+#z = velocity/c, velocity = hubbleconstant(roughly 70)*distance
 
-        velocity = z * 2997992458
-        radius = velocity/70
+listBlzInfo = []
+for blazar in blazar1:
+    BlzRA = blazar["ra"]
+    BlzDE = float(blazar["de"])
+    BlzLon = RatoLon(BlzRA)
+    
+    BlzName = blazar["a"]
 
-        RADistance = difRA/360 * radius * 2 * math.pi
-        DEDistance = difDE/360 * radius * 2 * math.pi
+    theta = BlzLon
+    alpha = abs(BlzDE) - 90
 
-        distance = (RADistance ** 2 + DEDistance ** 2) ** 0.5
-        listBlzInfo.append({"dist" : distance, "name": BlzName})
+    
+    
+    difRA = NtrRA - BlzLon
+    difDE = NtrDE - BlzDE
+    RS = blazar['z']
 
-    smallestdistance = sorted(listBlzInfo, key=lambda x: x["dist"])[0]
-    print(smallestdistance)
+    velocity = RS * 2997992458
+    radius = velocity/70
 
-except Exception as e:
-    print(e)
+    RADistance = difRA/360 * radius * 2 * 3.1415
+    DEDistance = difDE/360 * radius * 2 * 3.1415
+
+    distance = (RADistance ** 2 + DEDistance ** 2) ** 0.5
+
+    x = cos(theta)*distance*sin(alpha)
+    y = sin(theta)*distance*sin(alpha)
+    z = distance*cos(alpha)
+    print(x,y,x)       
+    listBlzInfo.append({"dist" : distance, "name": BlzName})
+
+smallestdistance = sorted(listBlzInfo, key=lambda x: x["dist"])[0]
+print(smallestdistance)
+
+
+           
