62b57f9f19db47ac16cce60c1a7a97fc2ebbfdb6
Joshua Wang <jwanggt@gmail.com>
2019-04-28 18:31:01 -0400

    Create app.py

---
+++ web/app.py
@@ -0,0 +1,37 @@
+import NeutrinoBlazarMatching as nbm
+
+from flask import Flask, request, redirect
+from flask_cors import CORS, cross_origin
+
+import json
+
+app = Flask(__name__)
+
+cors = CORS(app)
+
+app.config["CORS_HEADERS"] = "Content-Type"
+
+@app.route("/")
+def index():
+  return "hello"
+
+@app.route("/getResults", methods=["GET", "POST"])
+def getResults():
+  if request.method == "POST":
+    d = json.loads(request.data.decode("utf-8"))
+
+    ra = float(d["ra"])
+    de = float(d["dec"])
+
+    a, v, t = nbm.main(ra, de, verbose=False)
+    
+    ret = json.dumps(v)
+
+    print(ret)
+
+    return ret
+
+  else:
+    return redirect("https://banjo.joshdabosh.repl.co/",code=200)
+
+app.run(host="0.0.0.0", port=7979)
